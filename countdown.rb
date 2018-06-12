6d7563b690a9050ec4f8c9800ec680349430cd81
Kevin Chi <kevinchi118@gmail.com>
2018-06-12 19:10:21 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
 #write your code here
 
-def countdown
+def countdown(integer)
+    while integer > 0
+        if integer == 1
+            puts "#{integer} SECOND!"
+        else puts "#{integer} SECONDS!"
+        end
+        integer -= 1
+    end
+    return "HAPPY NEW YEAR!"
 end
